83b73aae7a02dd038cb3e015d75310e9bae0f4d0
Korbinian Maier <korbinian.maier@tum.de>
2020-04-06 16:52:05 +0200

    test/ci: fail on clippy warnings, check all tests and features

--- src/byte_offset.rs
+++ src/byte_offset.rs
@@ -262,33 +262,6 @@ impl std::cmp::Ord for ByteOffset {
     }
 }
 
-// Old Stuff
-// fn deserialize_suffix( n: &str) -> Result<u64>
-// {
-//     Ok(match n {
-//         "Ki" => 1024,
-//         "Mi" => 1024*1024,
-//         "Gi" => 1024*1024*1024,
-//         "K" => 1000,
-//         "M" => 1000*1000,
-//         "G" => 1000*1000*1000,
-//         "" => 1,
-//         n => return Err(
-//                 ScalpelError::ArgumentError
-//                 .context(format!("Bad Suffix: {}", n))
-//                 .into(),
-//             )
-//     })
-// }
-
-// pub fn serialize_cmd_opt(flag: String) -> Result<u64> {
-
-//     let suffix: u64 = deserialize_suffix(flag.trim_matches(char::is_numeric))?;
-//     let val: u64 = flag.trim_matches(char::is_alphabetic).parse()?;
-
-//     Ok(val * suffix)
-// }
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -321,7 +294,7 @@ mod test {
 
         assert_eq!(byte_offsets[0], ByteOffset::new(0, Magnitude::Unit));
         assert_eq!(byte_offsets[1], ByteOffset::new(256, Magnitude::Unit));
-        assert_eq!(byte_offsets[2], ByteOffset::new(1027261, Magnitude::Unit));
+        assert_eq!(byte_offsets[2], ByteOffset::new(1_027_261, Magnitude::Unit));
     }
 
     #[test]
